a527d46d5018fcdc2f5f5e8ac8bf7333b39d4b9c
AdorablePuddle <45329320+AdorablePuddle@users.noreply.github.com>
2026-07-03 11:54:57 +1000

    It technically works. Technically.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,40 +1,203 @@
-import { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Camera, useCameraDevice, useCameraPermission, useFrameOutput } from "react-native-vision-camera";
+import { useEffect, useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Camera, useAsyncRunner, useCameraDevice, useCameraPermission, useFrameOutput } from "react-native-vision-camera";
+import { createSynchronizable } from "react-native-worklets";
 
 export default function Index() {
+	const [isMeasuring, setIsMeasuring] = useState(false);
+	const [currentHeartrate, setCurrentHeartrate] = useState(0);
+	
+	const frameValues = useRef<number[]>([]);
+	const timestamps  = useRef<number[]>([]);
+
+	const synchronizableFrameValues = createSynchronizable<number[]>([]);
+	const synchronizableTimestamps  = createSynchronizable<number[]>([]);
+
 	const { hasPermission, requestPermission } = useCameraPermission();
 	const device = useCameraDevice("back");
+	const asyncRunner = useAsyncRunner();
 
 	useEffect(() => {
 		if (!hasPermission) requestPermission();
 	}, [hasPermission, requestPermission]);
+
+	const measurement = (avgRed : number, frameTiming : number) => {
+		'worklet'
+		frameValues.current.push(avgRed);
+		timestamps.current.push(frameTiming);
+
+		while (timestamps.current[0] < frameTiming - 5000) { // Delete anything that exists more than 5 seconds ago.
+			frameValues.current.shift();
+			timestamps.current.shift();
+		}
+	}
+
+	/*
+	const frameOutput = useFrameOutput({
+		pixelFormat : "rgb",
+		onFrame(frame) {
+			'worklet'
+			const wasHandled = asyncRunner.runAsync(() => {
+				'worklet'
+				// console.log(`Received ${frame.width}x${frame.height} Frame!`);
+				try {
+					const frameTiming = Date.now();
+					const buffer = frame.getPixelBuffer();
+					const pixels = new Uint8Array(buffer);
+
+					// const firstPixel = {a: pixels[3], r : pixels[2], g : pixels[1], b : pixels[0]}
+					var totalRed = 0;
+					var total = 0;
+					for (var i = 0; i + 3 < pixels.length; i += 4) {
+						const currentRed = pixels[i + 2];
+						total += 1;
+						totalRed += currentRed;
+					}
+					runOnJS(measurement)((total > 0)? totalRed / total : 0, frameTiming);
+				} catch (error) {
+					console.error(`Frame Processing Error: ${error}`)
+				} finally {
+					frame.dispose();
+				}
+			});
+			if (!wasHandled) {
+				frame.dispose();
+			}
+		}
+	})
+	*/
+	/*
+	const frameOutput = useFrameOutput({
+		pixelFormat : "rgb",
+		onFrame(frame) {
+			'worklet'
+			// console.log(`Received ${frame.width}x${frame.height} Frame!`);
+			try {
+				const frameTiming = Date.now();
+				const buffer = frame.getPixelBuffer();
+				const pixels = new Uint8Array(buffer);
+
+				// const firstPixel = {a: pixels[3], r : pixels[2], g : pixels[1], b : pixels[0]}
+				var totalRed = 0;
+				var total = 0;
+				for (var i = 0; i + 3 < pixels.length; i += 4) {
+					const currentRed = pixels[i + 2];
+					total += 1;
+					totalRed += currentRed;
+				}
+
+				// console.log(`Avg Red: ${(total > 0)? totalRed / total : 0}`)
+				// setCurrentAvg((total > 0)? totalRed / total : 0);
+				frameValues.current.push((total > 0)? totalRed / total : 0);
+				timestamps.current.push(frameTiming);
+
+				while (timestamps.current[0] < frameTiming - 5000) { // Delete anything that exists more than 5 seconds ago.
+					frameValues.current.shift();
+					timestamps.current.shift();
+				}
+			} catch (error) {
+				console.error(`Frame Processing Error: ${error}`)
+			} finally {
+				frame.dispose();
+			}
+		}
+	});
+	*/
+
+	const measureToggler = () => {
+		setIsMeasuring(!isMeasuring);
+	}
 
 	const frameOutput = useFrameOutput({
 		pixelFormat : "rgb",
 		onFrame(frame) {
 			'worklet'
 			try {
+				const frameTiming = Date.now();
+				const frameValuesBlocking = synchronizableFrameValues.getBlocking();
+				const timestampsBlocking  = synchronizableTimestamps.getBlocking();
 				const buffer = frame.getPixelBuffer();
+				const pixels = new Uint8Array(buffer);
+
+				// const firstPixel = {a: pixels[3], r : pixels[2], g : pixels[1], b : pixels[0]}
+				var totalRed = 0;
+				var total = 0;
+				for (var i = 0; i + 3 < pixels.length; i += 4) {
+					const currentRed = pixels[i + 2];
+					total += 1;
+					totalRed += currentRed;
+				}
+
+				// console.log(`Avg Red: ${(total > 0)? totalRed / total : 0}`)
+				// setCurrentAvg((total > 0)? totalRed / total : 0);
+
+				synchronizableFrameValues.setBlocking([...frameValuesBlocking, (total > 0)? totalRed / total : 0]);
+				synchronizableTimestamps.setBlocking([...timestampsBlocking, frameTiming]);
+			} catch (error) {
+				console.error(`Frame Processing Error: ${error}`)
 			} finally {
 				frame.dispose();
 			}
 		}
-	})
+	});
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			const currentFrameValues = synchronizableFrameValues.getBlocking();
+			const currentTimestamps  = synchronizableTimestamps.getBlocking();
+			synchronizableFrameValues.setBlocking([]);
+			synchronizableTimestamps.setBlocking([]);
+
+			const data = currentFrameValues.map((e, i) => [e, currentTimestamps[i]]);
+			// Naive Peak Analysis:
+			// SKIP IF EMPTY
+			if (data.length == 0) return;
+			const sorted_data = data.sort((a, b) => a[1] - b[1])
+			const startTime = sorted_data[0][1];
+			const endTime = sorted_data[sorted_data.length - 1][1];
+			const timeTaken = endTime - startTime; // in MS
+			const timeTakenMinutes = (timeTaken / 1000) / 60;
+
+			var localPeaks = 0;
+			for (var i = 1; i + 1 < sorted_data.length; i += 1) {
+				localPeaks += (sorted_data[i][0] > sorted_data[i + 1][0] && sorted_data[i][0] > sorted_data[i - 1][0])? 1 : 0;
+			}
+
+			const currentBPM = localPeaks / timeTakenMinutes;
+			// console.log(`Interval Marker: ${currentBPM}`);
+			if (Number.isNaN(currentBPM)) return;
+			setCurrentHeartrate(currentBPM);
+		}, 5000);
+
+		return () => clearInterval(intervalId);
+	});
 
 	if (device)
 		return (
-			<Camera 
-				style = {{flex : 1}}
-				isActive = {true}
-				device = "back"
-				outputs = {[frameOutput]}
-			/>
+			<View style = {styles.container}>
+				<View style = {styles.camera_container}>
+					<Camera 
+						style = {styles.camera_hider}
+						isActive = {isMeasuring}
+						device = {device}
+						outputs = {[frameOutput]}
+					/>
+				</View>
+				<View style = {styles.data_container}>
+					<Text style = {styles.data_title}>Heartrate Monitor Prototype</Text>
+					<Button
+						onPress = {measureToggler}
+						title = {!isMeasuring? "Begin Measuring" : "End Measuring"}
+					/>
+					<Text style = {styles.data_text}>Note: To begin measurement, cover the back camera with your index finger lightly. Do not press it too tightly, but not too lightly either.</Text>
+					<Text style = {styles.data_title}>BPM: {Math.round(currentHeartrate)}</Text>
+				</View>
+			</View>
 		);
 	else {
 		return (
 			<View style = {styles.container}>
-				<Text>Camera not found :(</Text>
+				<Text style = {styles.data_title}>Camera not found :(</Text>
 			</View>
 		)
 	}
@@ -42,11 +205,34 @@ export default function Index() {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
+		flex : 1,
+		flexDirection : "column",
+		width : "100%",
+		height : "100%",
+		backgroundColor : "#333",
+	},
+	camera_container: {
+		flex : 0,
 	},
 	camera_hider: {
-		
+		flex : 1,
+		opacity : 0,
 	},
+	data_container: {
+		flex : 1,
+		alignItems : "center",
+		justifyContent : "center",
+	},
+	data_title: {
+		textAlign : "center",
+		color: "#F5A9B8",
+		fontWeight: "bold",
+		fontSize : 25,
+	},
+	data_text: {
+		textAlign : "center",
+		color: "#5BCEFA",
+		fontStyle : "italic",
+		fontSize : 12,
+	}
 });
